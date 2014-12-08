0385f77fd0610c59f2338f5be8474dd1286fa25c
Hosomy17 <jackson.daniel.17dh@gmail.com>
2014-12-08 15:05:06 -0200

    Tutorial

---
+++ Unity/Assets/Scripts/ControleCarapana.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControleCarapana : MonoBehaviour {
+
+	private Vector2 _minTela;
+	private Vector2 _maxTela;
+	private Vector2 _novaPosicao;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		_minTela = new Vector2(-11f,-12f);
+		_maxTela = new Vector2(11f,1f);
+		speed = 2f;
+		Invoke("NovaPosicao",0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position,_novaPosicao, Time.deltaTime * speed);
+	}
+
+	void NovaPosicao(){
+		float aleatorioX = Random.Range(_minTela.x,_maxTela.x);
+		float aleatorioY = Random.Range(_minTela.y,_maxTela.y);
+		_novaPosicao = new Vector2(aleatorioX,aleatorioY);
+		Invoke("NovaPosicao",Random.Range(1f,speed));
+	}
+
+	void OnCollisionEnter2D(Collision2D c){
+		if(c.gameObject.CompareTag("dardo")){
+			Kill();
+		}
+	}
+
+	void Kill(){
+		Destroy(gameObject);
+	}
+}
---
+++ Unity/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour {
+
+	private Vector2 _minTela;
+	private Vector2 _maxTela;
+	public GameObject carapana;
+	private GameObject _carapana;
+	public GameObject personagem;
+	private GameObject _personagem;
+	
+	// Use this for initialization
+	void Start () {
+		_minTela = new Vector2(-11,-12);
+		_maxTela = new Vector2(11,1);
+		CriaPersonagem();
+		CriaCarapana();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CriaPersonagem()
+	{
+		_personagem = GameObject.Instantiate(personagem)as GameObject;
+		Controles controle = _personagem.GetComponent<Controles>();
+		controle.enabled = true;
+		SeguidorCamera camera = _personagem.GetComponent<SeguidorCamera>();
+		camera.enabled = true;
+
+		VirtualJoystick joy = _personagem.GetComponent<VirtualJoystick>();
+		joy.enabled = true;
+
+	}
+	public void CriaCarapana(){
+		_carapana = GameObject.Instantiate(carapana) as GameObject;
+		float aleatorioX = Random.Range(_minTela.x,_maxTela.x);
+		float aleatorioY = Random.Range(_minTela.y,_maxTela.y);
+		Vector2 posicao = new Vector2(aleatorioX,aleatorioY);
+		_carapana.transform.position = posicao;
+	}
+}
